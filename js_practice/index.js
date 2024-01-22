b2b55d920a40a88fa89490eaea3543d825186e5b
khushi <khushi.p@knackroot.com>
2024-01-22 07:56:11 +0530

    second commit

--- js_practice/index.js
+++ js_practice/index.js
@@ -59,3 +59,56 @@ let fname = "f.name";
 let lname = "l.name";
 let fullname = `${fname} ${lname}`;
 console.log(fullname)
+
+
+
+//array
+var arr1 = ["hello", "world", 2];
+console.log(arr1);
+//access array using index
+console.log(arr1[1]);
+console.log(arr1.length);
+//change the value of array 
+arr1[0] = 1;
+console.log(arr1)
+
+// to add the element at the end of the array use push
+arr1.push("Hello");
+console.log(arr1)
+//to remove the element at the end of the array use pop
+arr1.pop();
+console.log(arr1)
+
+//to remove the element at the start of the array use shift
+console.log(arr1.shift())
+console.log(arr1)
+
+//to add the element at the start of the array use unshift
+arr1.unshift(1);
+console.log(arr1)
+
+
+
+//scope
+var global_num = 10;
+
+function fun1() {
+    // oops_global = 5; //consider as a global 
+    var oops_global = 5; //consider as a local 
+}
+
+function fun2() {
+    var output = "";
+    if(typeof global_num != "undefined"){
+        output += "global_num = " + global_num;
+    }
+    if(typeof oops_global != "undefined"){
+        output += "oops_global = "+ oops_global;
+    }
+    console.log(output)
+}
+
+fun1();
+fun2();
+
+
